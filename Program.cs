3e3ac054c3ade114f79748bdb418578052af7955
Limuncic <145533801+Limuncic@users.noreply.github.com>
2023-10-25 08:57:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaString4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Unesi neku recenicu: ");
+            string s1= Console.ReadLine();
+
+            string[] sRijec = s1.Split(' ');
+            
+
+            Console.WriteLine("Prva rijec je: " + sRijec[0]);
+            Console.WriteLine("Prva rijec je: " + sRijec[sRijec.Length-1]);
+
+            Console.ReadKey();
+        }
+    }
+}
